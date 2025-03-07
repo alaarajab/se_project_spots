9d0ac449345fa158b0083bc93cccf76a8a863686
Alaa Rajab <alaa88rajab@gmail.com>
2025-03-06 22:50:22 -0600

    Add changes

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,26 +1,48 @@
 const showInputError = (formEl, inputEl, errorMsg) => {
   const errorMsgEl = document.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.add("modal__input_type_error");
   errorMsgEl.textContent = errorMsg;
+};
+const hideInputError = (formEl, inputEl) => {
+  const errorMsgEl = document.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.remove("modal__input_type_error");
+  errorMsgEl.textContent = "";
 };
 
 const checkInputValidity = (formEl, inputEl) => {
   if (!inputEl.validity.valid) {
     showInputError(formEl, inputEl, inputEl.validationMessage);
+  } else {
+    hideInputError(formEl, inputEl);
+  }
+};
+const hasInvalidInput = (inputList) => {
+  return inputList.some((input) => {
+    return !input.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonEl) => {
+  // console.log(hasInvalidInput(inputList));
+  if (hasInvalidInput(inputList)) {
+    buttonEl.disabled = true;
+    buttonEl.classList.add("btn_inactive");
+  } else {
+    buttonEl.disabled = false;
+    buttonEl.classList.remove("btn_inactive");
   }
 };
 const setEventListeners = (formEl) => {
   const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
-  // const inputList = formEl.querySelectorAll(".modal__input");
   const buttonElement = formEl.querySelector(".modal__submit-btn");
 
-  // here, to check the button state in the very beginning
-  // toggleButtonState(inputList, buttonElement);
+  toggleButtonState(inputList, buttonEl);
 
   inputList.forEach((inputEl) => {
     inputEl.addEventListener("input", function () {
       checkInputValidity(formEl, inputEl);
-      // and here, to check it whenever any field input is changed
-      // toggleButtonState(inputList, buttonElement);
+
+      toggleButtonState(inputList, buttonEl);
     });
   });
 };
